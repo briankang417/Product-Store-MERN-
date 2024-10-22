7789123d2f6c82e435bfd7e64618d2f93707af8f
Brian Kang <brianjkang4@gmail.com>
2024-10-22 14:04:30 -0700

    Created userSchema for user Reg System

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -4,6 +4,12 @@ import { createProduct, deleteProduct, getProducts, updateProduct } from "../con
 
 const router = express.Router();
 
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/user.model.js');
+
 //all prefixed with "/api/products"
 
 //fetch all products in database
